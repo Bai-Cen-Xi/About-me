b348819323693061a70cd1c9ce6f8a58db1c6e34
Bai-Cen-Xi <73214659+Bai-Cen-Xi@users.noreply.github.com>
2023-05-06 13:40:18 +0800

    Add files via upload
    我的网站，源码下载（雏形）

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+// var qwe = document.querySelectorAll('.img-box');
+// var asd = document.querySelectorAll('.have-box');
+// qwe.forEach((v, i) => {
+//     v.onmouseover = () => {
+//         // asd[i].style.display = "block"
+//         asd[i].style.opacity = 1
+//     }
+//     v.onmouseout = () => {
+//         // asd[i].style.display = "none"
+//         asd[i].style.opacity = 0
+//     }
+// })
